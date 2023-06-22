204d44801d598bf5bc179e46618ddfb43b80fb7b
GuessWhatBBQ <saminslm@gmail.com>
2023-06-22 15:47:50 +0600

    added file helper

---
+++ src/Helpers/File.cs
@@ -0,0 +1,12 @@
+namespace Imagegram.Helpers.File;
+
+public class FileHelper
+{
+    public static string GetUniqueFileName(string fileName)
+    {
+        return string.Concat(Path.GetFileNameWithoutExtension(fileName)
+                            , "_"
+                            , Guid.NewGuid().ToString()
+                            , Path.GetExtension(fileName));
+    }
+}
